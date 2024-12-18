423dcb600737bcbc07bbcf782bc548adff6ec070
mahadtkxel <mahad.karim@tkxel.io>
2024-12-18 17:09:45 +0500

    testing params

---
+++ params.groovy
@@ -0,0 +1,9 @@
+return [
+    BRANCH_NAME = "master",
+    TOOL_NAME = "sonarqube",
+    SONAR_PROJECT = "tkxelassignment1",
+    IMAGE_TAG = "tkxelassignment_1",
+    DOCKER_FILE = "dev.Dockerfile",
+    GIT_URL = "https://github.com/Mahadtkxel/python-project-blueprint.git",
+    
+]
